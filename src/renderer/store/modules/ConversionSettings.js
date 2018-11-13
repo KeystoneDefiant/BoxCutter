dd70225c04236d758ba07e2c42b3582b7c8a183b
Chris <chris@port14.com>
2018-11-12 21:06:12 -0500

    Added conversion settings

--- src/renderer/store/modules/ConversionSettings.js
+++ src/renderer/store/modules/ConversionSettings.js
@@ -1,49 +1,53 @@
 const state = {
 	conversionMatrix:{
-		retropie:{
-			"3do":"",
-			"amiga":"Commodore Amiga",
-			"amstradcpc":"Amstrad CPC",
-			"apple2":"Apple 2",
-			"art":"",
-			"atari2600":"Atari 2600",
-			"atari5200":"Atari 5200",
-			"atari7800":"Atari 7200",
-			"atari800":"Atari 800",
-			"atarijaguar":"Atari Jaguar",
-			"atarijaguarcd":"Atari Jaguar CD",
-			"atarist":"Atari ST",
-			"atarifalcon":"Atari Falcon",
-			"atarixe":"Atari XE",
-			"c64":"Commodore 64",
-			"colecovision":"ColecoVision",
-			"amstradcpc":"Amstrad CPC",
-			"fba":"Final Burn Alpha",
-			"gamegear":"Sega Game Gear",
-			"gb":"Nintendo Game Boy",
-			"gba":"Nintendo Game Boy Advance",
-			"gbc":"Nintendo Game Boy Color",
-			"gc":"Nintendo GameCube",
-			"intellivision":"Mattel Intellivision",
-			"macintosh":"Apple Macintosh",
-			"mame":"Arcade",
-			"mastersystem":"Sega Master System",
-			"megadrive":"Sega Genesis",
-			"n64":"Nintendo 64",
-			"neogeo":"",
-			"nes":"Nintendo Entertainment System",
-			"ngp":"SNK Neo Geo Pocket",
-			"ngpc":"SNK Neo Geo Pocket Color",
-			"pc":"DOS",
-			"pcengine":"NEC TurboGrafx016",
-			"ports":"",
-			"psx":"Sony Playstation",
-			"scummvm":"ScummVM",
-			"sega32x":"Sega 32X",
-			"segacd":"Sega CD",
-			"snes":"Super Nintendo Entertainment System",
-			"zmachine":"",
-			"zxspectrum":"ZX Spectrum",
+		"retropie":{
+			"Arcade":"mame",
+			"Atari 2600":"atari2600",
+			"Atari 5200":"atari5200",
+			"Atari 7800":"atari7800",
+			"Atari Jaguar":"atarijaguar",
+			"Atari Jaguar CD":"atarijaguarcd",
+			"ColecoVision":"colecovision",
+			"Commodore Amiga":"amiga",
+			"GCE Vectrex":"vectrex",
+			"Mattel Intellivision":"intellivision",
+			"NEC TurboGrafx-16":"pcengine",
+			"NEC TurboGrafx-CD":"",
+			"Nintendo 64":"n64",
+			"Nintendo Entertainment System":"nes",
+			"Nintendo Game Boy":"gb",
+			"Nintendo Game Boy Advance":"gba",
+			"Nintendo Game Boy Color":"gbc",
+			"Nintendo GameCube":"gc",
+			"Nintendo Wii":"wii",
+			"Philips CD-i":"",
+			"Sega 32X":"sega32x",
+			"Sega CD":"segacd",
+			"Sega Dreamcast":"dreamcast",
+			"Sega Game Gear":"gamegear",
+			"Sega Genesis":"megadrive",
+			"Sega Master System":"mastersystem",
+			"Sega Saturn":"saturn",
+			"SNK Neo Geo Pocket Color":"ngp",
+			"SNK Neo Geo Pocket Color":"ngpc",
+			"Sony Playstation":"psx",
+			"Sony Playstation 2":"ps2",
+			"Sony PSP":"psp",
+			"Super Nintendo Entertainment System":"snes",
+			"Windows":"",
+			"Amstrad CPC":"amstradcpc",
+			"Apple 2":"apple2",
+			"Atari 800":"",
+			"Atari ST":"atarist",
+			"Atari Falcon":"atarifalcon",
+			"Atari XE":"atarixe",
+			"Commodore 64":"c64",
+			"Amstrad CPC":"amstradcpc",
+			"Final Burn Alpha":"fba",
+			"Apple Macintosh":"macintosh",
+			"DOS":"pc",
+			"ScummVM":"scummvm",
+			"ZX Spectrum":"zxspectrum",
 		}
 	}
 }
@@ -56,8 +60,19 @@ const actions = {
 
 }
 
+const getters = {
+	conversions: state => {
+		return state.conversionMatrix
+	},
+
+	list: (state) => (searchName) =>{
+		return state.conversionMatrix.find(list => conversionMatrix.name === searchName);
+	},
+}
+
 export default {
 state,
 mutations,
-actions
+actions,
+getters
 }
